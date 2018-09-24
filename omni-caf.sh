825247feb90d3d5cf34f28f9c9839fde4748c36e
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2018-09-24 06:00:32 +0000

    update omni script

--- omni-caf.sh
+++ omni-caf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source $HOME/omni/build/envsetup.sh
+source build/envsetup.sh
 croot
 
 rm -rf external/tinycompress
@@ -35,46 +35,53 @@ git clone https://github.com/PixelExperience-P/hardware_qcom_wlan.git  -b pie-ca
 rm -rf hardware/ril-caf
 git clone https://github.com/PixelExperience-P/hardware_ril.git -b pie-caf hardware/ril-caf
 
+git clone https://github.com/zeelog/android_device_xiaomi_mido.git -b lineage-16.0 device/xiaomi/mido
+git clone https://github.com/zeelog/android_kernel_xiaomi_msm8953.git -b lineage-16.0 kernel/xiaomi/msm8953
+git clone https://github.com/zeelog/proprietary_vendor_xiaomi.git -b lineage-16.0 vendor/xiaomi
 
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_audio -b lineage-16.0 vendor/qcom/opensource/audio
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_dataservices -b lineage-16.0 vendor/qcom/opensource/dataservices
 git clone https://github.com/LineageOS/android_vendor_qcom_opensource_interfaces -b lineage-16.0 vendor/qcom/opensource/interfaces
-git clone https://github.com/LineageOS/android_vendor_qcom_opensource_dataservices -b lineage-16.0 vendor/qcom/opensource/android_vendor_qcom_opensource_dataservices
-git clone https://github.com/LineageOS/android_vendor_qcom_opensource_audio -b lineage-16.0 vendor/qcom/opensource/audio-caf
-
-if [[ ! -d system/qcom ]]; then
-	git clone https://github.com/PixelExperience-P/system_qcom -b pie system/qcom	
-fi
-
-croot
-if [[ ! -d device/xiaomi/msm8953-common ]]; then
-	git clone https://github.com/TheScarastic/android_device_xiaomi_msm8953-common -b lineage-16.0 device/xiaomi/msm8953-common	
-fi
-if [[ ! -d device/xiaomi/mido ]]; then
-	git clone https://github.com/TheScarastic/android_device_xiaomi_mido -b lineage-16.0 device/xiaomi/mido 	
-fi
-if [[ ! -d kernel/xiaomi/msm8953 ]]; then
-	git clone https://github.com/TheScarastic/android_kernel_xiaomi_msm8953 -b lineage-15.1-treble kernel/xiaomi/msm8953 	
-fi
-if [[ ! -d vendor/xiaomi ]]; then
-	git clone https://github.com/TheScarastic/proprietary_vendor_xiaomi -b lineage-16.0 vendor/xiaomi	
-fi
+git clone https://github.com/PixelExperience-P/system_qcom -b pie system/qcom
 
 # Merge frameworks stuff
-cd $HOME/omni/frameworks/native 
-
-git fetch https://github.com/PixelExperience-P/frameworks_native pie 
+cd $HOME/omni/frameworks/native
+git fetch https://github.com/PixelExperience-P/frameworks_native pie
 git cherry-pick 911a39edc9356ec63a2f84f530bbf65a6da7810a^..d4dc135e3f8ff8cd824786666e239eb489b601e5
-
 croot
 
 #merge bionic
-cd $HOME/omni/bionic 
-git fetch https://github.com/PixelExperience-P/bionic.git pie 
-git cherry-pick 067774cb3f49b80f4d9bd7fc488c7e15c1151e90^..53cb94ec41f463a6e7c0e2d857b7d52342ab838f
-git cherry-pick 34b8be6662365e6ca98859bd990cec8b54cad335^..57fc114e0d3f38b5d97ba4011af7f196096ff1eb
+#cd $HOME/omni/bionic
+#git fetch https://github.com/PixelExperience-P/bionic.git pie
+#git cherry-pick 067774cb3f49b80f4d9bd7fc488c7e15c1151e90^..53cb94ec41f463a6e7c0e2d857b7d52342ab838f
+#git cherry-pick 34b8be6662365e6ca98859bd990cec8b54cad335^..57fc114e0d3f38b5d97ba4011af7f196096ff1eb
+#croot
+
+
+#project path for mido+hals
+
+cd device/xiaomi/mido/camera/Q*
+
+sed -i "s|\$(call project-path-for,qcom-media)|hardware\/qcom\/media-caf\/msm8996|g" Android.mk
+sed -i "s|\$(call project-path-for,qcom-display)|hardware\/qcom\/display-caf\/msm8996|g" Android.mk
+
+croot
+#cd $HOME/omni/hardware/qcom/audio-caf/msm8996
+#find . -name '*.mk' -exec grep -i 'call project' {} \; -print -exec sed -i "s|\$(call project-path-for,qcom-audio)|hardware\/qcom\/audio-caf\/msm8996|g" {} \;
+
+#cd $HOME/omni/hardware/qcom/display-caf/msm8996
+#find . -name '*.mk' -exec grep -i 'call project' {} \; -print -exec sed -i "s|\$(call project-path-for,qcom-display)|hardware\/qcom\/display-caf\/msm8996|g" {} \;
+
+#cd $HOME/omni/hardware/qcom/media-caf/msm8996
+#find . -name '*.mk' -exec grep -i 'call project' {} \; -print -exec sed -i "s|\$(call project-path-for,qcom-media)|hardware\/qcom\/media-caf\/msm8996|g" {} \;
 
 croot
 
-# lunch mido
+for hal in audio display media; do
+	cd $HOME/omni/hardware/$hal-caf/msm8996
+	find . -name '*.mk' -exec grep -i 'call project' {} \; -print -exec sed -i "s|\$(call project-path-for,qcom-$hal)|hardware\/qcom\/$hal-caf\/msm8996|g" {} \;
+done
+
+# finally lunch and exit +_+ let user beeld eet
 
 lunch omni_mido-userdebug
-
